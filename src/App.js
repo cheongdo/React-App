dae44fd1be6a8ca2024311bb8ed7ee20491206c5
systaem <taehyun36850@gmail.com>
2021-04-22 13:34:05 +0900

    30%

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+
+function Food(props){
+  return <h1>Korean Food {props.fav }{props.hel} </h1>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>HELLO!</h1>
+        <Food fav="kimch" />
+        <Food fav="ramen" />
+        <Food fav="hanbok" />
+        <Food hel="hello" />
+        
+      
     </div>
-  );
+    )
 }
 
 export default App;
