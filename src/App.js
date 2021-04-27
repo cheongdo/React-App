e2335905b9dc619cd68960067e8c5f5e4af3e767
systaem <taehyun36850@gmail.com>
2021-04-27 10:05:14 +0900

    start creating apps

--- src/App.js
+++ src/App.js
@@ -1,38 +1,9 @@
 import React from 'react';
 
-
 class App extends React.Component {
-  state = {
-    count: 0,
-
-  };
-
-  add = () => {
-    this.setState(current => ({
-      count: current.count + 1,
-    }));
-  };
-
-  minus = () => {
-    this.setState(curr => ({
-      count: curr.count - 1,
-    }));
-  };
-
-
-
-
-  render(){
-
-   return (
-     <div>
-   <h3>This count is: {this.state.count}</h3>
-   <button onClick={this.add}>Add</button>
-   <button onClick={this.minus}>Minus</button>
-   </div>
-   )
+  render() {
+    return <div />;
   }
-
 }
 
 export default App;
